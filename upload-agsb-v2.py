ae9b51b90e3a42c960ab0e62dfe8e5e971fd5c15
man622 <yang76270@gmail.com>
2025-06-07 09:25:16 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "bigpenisl"         # 用户名
+UUID = "4149861b-d0c2-4314-a92a-1539ef06bf73"                     # UUID，留空则自动生成
+PORT = 26892                   # Vmess端口，留空或0则自动生成
+DOMAIN = "man622streamlit.manbf.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiYjYxMWRlYmIzNmMwY2E4N2IzNmMxOTliYTQ1NmJjNjciLCJ0IjoiN2QyNTRkNWQtNjg4Yi00YjUxLTk4ZTktY2ZkMGQyYjYyYzBkIiwicyI6IllUZG1OMlZoTURrdE9XRTRaaTAwTkRVNExUaG1ObUV0WlRrellXRmxPR1l5WVRWaSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
